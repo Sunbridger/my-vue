642c7c404892a744e91e74f88739dbe18f69c14d
Sunbridger <739272884@qq.com>
2019-07-24 13:34:20 +0800

    增加ts的

--- src/components/Retive.vue
+++ src/components/Retive.vue
@@ -19,15 +19,27 @@ interface per {
     age: number;
     id: number;
 }
+
 @Component
 export default class Retive extends Vue {
-    table =  <per[]>[];
+    table =  <per[]>[]; 
+    // <per[]>[] 的解释
+    // 1.可以在元素类型后面接上 []，表示由此类型元素组成的一个数组： per[] 
+    // 2.类型断言有两种形式。 其一是“尖括号”语法： <per[]>[] 后面的空数组是我断言的 
+    a:per[]
     mounted() {
         this.table = [
             {
                 id: 999,
                 age: 1,
                 name: 'xunbridge'
+            }
+        ];
+        this.a = [
+            {
+                name:'xxx',
+                age:2,
+                id:8
             }
         ]
     };

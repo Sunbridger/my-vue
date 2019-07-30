db63ad9d22025ba0e84998b300478514093dda1b
Sunbridger <739272884@qq.com>
2019-07-30 23:49:00 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
-
+import VueRouter from 'vue-router';
+import Data from './components/Data';
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/a',
+      component: Data
+    }
+  ]
+})
 
 new Vue({
   render: h => h(App)
